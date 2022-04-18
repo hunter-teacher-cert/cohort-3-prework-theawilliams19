e120bdb0ec3740b356b32e17853e18d7c3bcb775
theawilliams19 <96701363+theawilliams19@users.noreply.github.com>
2022-04-18 21:32:23 +0000

    Fixed line breaks, spacing and verb/noun agreement for 1 bottle verse. Still have "0 bottle" agreement issue.

--- pre04/Beer.java
+++ pre04/Beer.java
@@ -3,16 +3,22 @@ import java.util.*;
 
 public class Beer {
   
-  public static void lyrics (int beer){
-    if (beer>0){  
-    System.out.print(beer+" 99 bottles of beer on the wall,");
-    System.out.print(beer+" 99 bottles of beer,ya’ take one down, ya’ pass it around, ");
-    System.out.println(beer-1+" bottles of         beer on the wall.");
-    lyrics(beer-1);}     
-      else {
-         System.out.println("No bottles of              beer on the wall, no bottles of beer,          ya' can't take one down, ya' can't             pass it around, 'cause there are no            more bottles of beer on the wall!");
+  public static void lyrics (int beer) {;
+    
+     if (beer>1){
+        System.out.print(beer +  " bottles of beer on the wall,\n");
+        System.out.print(beer+" bottles of beer,\nya’ take one down, ya’ pass it around,\n");
+        System.out.println(beer-1+" bottles of beer on the wall.\n");
+        lyrics(beer-1);
+       
+       } else if (beer==1){
+        System.out.print(beer +  " bottle of beer on the wall,\n");
+        System.out.print(beer+" bottle of beer,\nya’ take one down, ya’ pass it around,\n");
+        System.out.println(beer-1+" bottle of beer on the wall.\n");
+        lyrics(beer-1); 
+       } else if (beer==0)
+             System.out.println("No bottles of beer on the wall, \nno bottles of beer,\nya' can't take one down, ya' can't pass it around,/n'cause there are no more bottles of beer on the wall!");
   }
-}
 public static void main(String[] args){
     lyrics(99);
 }
